2bace1fdb251447d3d0c884da555730a94662c9e
Asad Ikhlas <33983874+asadikhlas@users.noreply.github.com>
2019-01-14 20:48:30 +0500

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,89 @@
+
+    <!--FOOER AREA-->
+    <footer class="footer-area sky-gray-bg relative">
+        <div class="footer-top-area padding-80-80 bg-dark">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-5 col-lg-5 col-sm-12 col-xs-12">
+                        <div class="single-footer footer-about sm-mb50 xs-mb50 sm-center xs-center">
+                            <div class="footer-logo mb30">
+                                <a href="http://pakeverzone.com/"><img src="assest/img/Capture.PNG" alt=""></a>
+                            </div>
+                            <p>Pak Ever Zone Group Group works in 1) Automobiles 2) Boutique 3) Cosmetic 4) Software house and Realestate. We successfully launched Travel Agency project and now we are moving in NGOs. </p>
+                            <br>
+                            <div class="single-footer footer-list white xs-center xs-mb50">
+                                <h3>Language Translator</h3>
+                                <div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-lg-2 col-sm-4 col-xs-12">
+                        <div class="single-footer footer-list white xs-center xs-mb50">
+                            <ul>
+                                <h3>Services</h3>
+                                <li><a href="index.php">Home</a></li>
+                                <li><a href="automobiles.php">Automobile</a></li>
+                                <li><a href="Boutique.php">Boutique</a></li>
+                                <li><a href="Cosmetic.php">Cosmetic</a></li>
+                                <li><a href="Realestate.php">Real Estate</a></li>
+                                <li><a href="Softwarehouse.php">Software House</a></li>
+                                <li><a href="Travelagency.php">Travel Agency</a></li>
+                                <li><a href="NGO.php">NGO</a></li>
+                                
+                                
+                               
+                                
+                               
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-5 col-lg-5 col-sm-8 col-xs-12">
+                        <div class="single-footer footer-subscribe white xs-center">
+                            <h3 class="mb30 xs-font18">Contact</h3>
+                            
+                            <p><i class="fa fa-map-marker"></i><b>Address: </b> SD-10 Suite No.38 Al Zehra Center Block G North Nazimabad Karachi</p>
+                            <p><i class="fa fa-envelope"></i><b> Email: </b> info@pakeverzone.com</p>
+                            <p><i class="fa fa-mobile"></i><b>&nbsp;&nbsp; Mobile: </b> 03242921388</p>
+                            <p><i class="fa fa-phone"></i><b>&nbsp; Telephone: </b> 02136676687</p>
+                            <p><i class="fa fa-user-secret"></i> &nbsp; Privacy Policy</p>
+                            <p> 
+                            <a href="https://www.facebook.com/pakeverzone" >
+                            <i class="fa fa-facebook" style="font-size: 25px; padding: 5px"></i></a> 
+                            <a href="https://twitter.com/pakeverzone?lang=bg"><i class="fa fa-twitter" style="font-size: 25px;padding: 5px"></i></a>
+                            <a href="https://www.linkedin.com/company/pak-ever-zone-pez/"><i class="fa fa-linkedin" style="font-size: 25px;padding: 5px"></i></a> </p>
+                            
+                            
+                            <div class="subscriber-form-area mt50 wow fadeIn">
+                                <!--<form action="#" class="subscriber-form mb100">
+                                    <input type="email" name="email" id="email" placeholder="Email Address">
+                                    <button type="submit">Subscribe</button>
+                                </form>-->
+                                <!-- <form id="mc-form" class="subscriber-form">
+                                    <label class="mt10" for="mc-email"></label>
+                                    <input type="email" id="mc-email" placeholder="email@example.com">
+                                    <button type="submit" class="plus-btn"><i class="fa fa-paper-plane-o"></i></button>
+                                </form> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom-area white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
+                        <div class="footer-copyright text-center wow fadeIn">
+                            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved by <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="http://www.pakeverzone.com/" target="_blank">Pak Ever Zone Group </a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
